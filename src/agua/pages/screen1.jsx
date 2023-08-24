1792a4efa75c0fb3d8a4ac22f000a808ec5d770c
david-dom <113909984+david-dom@users.noreply.github.com>
2023-08-24 07:39:39 -0500

    My commit message

---
+++ src/agua/pages/screen1.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './screen1.module.css';
+
+
+
+function Screen1() {
+    
+
+    return(
+        <div className={styles["Screen1"]}>
+            
+<div id="layer-1-c12d9b1921a44bf59f41915f05041ba1" className={styles["layer-1-c12d9b1921a44bf59f41915f05041ba1"]} >
+<div id="layer-8-db2e0a389f4d4d4ea652d4cea066830e" className={styles["layer-8-db2e0a389f4d4d4ea652d4cea066830e"]} >
+<img id="imageview-2-81e6b5d0151f4ac0a91cfce1df1b9691" className={styles["imageview-2-81e6b5d0151f4ac0a91cfce1df1b9691"]} alt="imageview-2" src='https://i.ibb.co/5ndGgWv/image-43.png' />
+</div>
+<div id="layer-9-0cb3beadb2e24287b0e9457c52a76632" className={styles["layer-9-0cb3beadb2e24287b0e9457c52a76632"]} >
+<div id="text-4-124f8f284a144e7e823e33fcff3e3998" className={styles["text-4-124f8f284a144e7e823e33fcff3e3998"]} >[Text]</div>
+</div>
+<div id="layer-10-61020b12b8704f05bcab2daffaa03070" className={styles["layer-10-61020b12b8704f05bcab2daffaa03070"]} >
+<div id="layer-11-3d13d8a8948b4a3db27b8b6197f0a558" className={styles["layer-11-3d13d8a8948b4a3db27b8b6197f0a558"]} >
+<div id="text-5-6eefe1fe64b6486293cbbf652aa609d6" className={styles["text-5-6eefe1fe64b6486293cbbf652aa609d6"]} >[Text]</div>
+</div>
+<div id="layer-12-870c8f2ec02c4b498afb0595b96df2d7" className={styles["layer-12-870c8f2ec02c4b498afb0595b96df2d7"]} >
+<div id="text-6-a44e649dead545cf95b4aa735122b28b" className={styles["text-6-a44e649dead545cf95b4aa735122b28b"]} >[Text]</div>
+</div>
+<div id="layer-13-1571db504fa44e66a59adc54c2023709" className={styles["layer-13-1571db504fa44e66a59adc54c2023709"]} >
+<div id="text-7-6dcd7e3819a94601b0f512ef2a2c0f49" className={styles["text-7-6dcd7e3819a94601b0f512ef2a2c0f49"]} >[Text]</div>
+</div>
+</div>
+</div>
+<div id="layer-2-ea657fcadb5c4a52811e4eff3a75f88d" className={styles["layer-2-ea657fcadb5c4a52811e4eff3a75f88d"]} >
+<div id="layer-3-21f2beb7d51748f79171f3a689a048d1" className={styles["layer-3-21f2beb7d51748f79171f3a689a048d1"]} >
+<div id="layer-4-b4e695c94cb94010a165e32d5a3def73" className={styles["layer-4-b4e695c94cb94010a165e32d5a3def73"]} >
+<div id="text-1-70bb91f7709c41038127a1a9c4fdd242" className={styles["text-1-70bb91f7709c41038127a1a9c4fdd242"]} >Noticias
+</div>
+<div id="layer-5-718754c027444384a7a471ec90c5c71d" className={styles["layer-5-718754c027444384a7a471ec90c5c71d"]} >
+<div id="layer-6-46c7deefbf12407aae5b7819f04d6715" className={styles["layer-6-46c7deefbf12407aae5b7819f04d6715"]} ></div>
+</div>
+</div>
+</div>
+</div>
+        </div>
+    );
+}
+
+export default Screen1;
+            
---
+++ src/agua/pages/screen2.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './screen2.module.css';
+
+
+
+function Screen2() {
+    
+
+    return(
+        <div className={styles["Screen2"]}>
+            
+<div id="layer-1-dbbe7b3cc8e74e4ab39eb087c49f966b" className={styles["layer-1-dbbe7b3cc8e74e4ab39eb087c49f966b"]} >
+<div id="layer-8-eded62c6f08c4b90adc80d852cdbecf5" className={styles["layer-8-eded62c6f08c4b90adc80d852cdbecf5"]} >
+<img id="imageview-2-6dc5354d13014cae991a5101776d6db2" className={styles["imageview-2-6dc5354d13014cae991a5101776d6db2"]} alt="imageview-2" src='https://i.ibb.co/5ndGgWv/image-43.png' />
+</div>
+<div id="layer-9-09c4b197aa604dc59d8dc9361c3d4ce5" className={styles["layer-9-09c4b197aa604dc59d8dc9361c3d4ce5"]} >
+<div id="text-4-7c88bb0b08b44b788325f3e64fcb31ab" className={styles["text-4-7c88bb0b08b44b788325f3e64fcb31ab"]} >[Text]</div>
+</div>
+<div id="layer-10-50d8bd865dca423d95830f67cc0d2e07" className={styles["layer-10-50d8bd865dca423d95830f67cc0d2e07"]} >
+<div id="layer-11-c4b6673d70a34af68be9bd833742322f" className={styles["layer-11-c4b6673d70a34af68be9bd833742322f"]} >
+<div id="text-5-711ab8c5874c44158bbf0916fbbd65ab" className={styles["text-5-711ab8c5874c44158bbf0916fbbd65ab"]} >[Text]</div>
+</div>
+<div id="layer-12-8909a54e45534dbfb063338eba60afc4" className={styles["layer-12-8909a54e45534dbfb063338eba60afc4"]} >
+<div id="text-6-dffe55572fe24779b2ec49c58aeb13eb" className={styles["text-6-dffe55572fe24779b2ec49c58aeb13eb"]} >[Text]</div>
+</div>
+<div id="layer-13-008cdda0d1094d6e8931d8034409a1ab" className={styles["layer-13-008cdda0d1094d6e8931d8034409a1ab"]} >
+<div id="text-7-d330097fcb4d42268c92f0312a9c7884" className={styles["text-7-d330097fcb4d42268c92f0312a9c7884"]} >[Text]</div>
+</div>
+</div>
+</div>
+<div id="layer-5-b9ef667fe27f46618d3eca312cfec658" className={styles["layer-5-b9ef667fe27f46618d3eca312cfec658"]} >
+<div id="layer-6-c94ce2ddc5464e10a52a52a1b6d08128" className={styles["layer-6-c94ce2ddc5464e10a52a52a1b6d08128"]} >
+<img id="imageview-1-1ec623ad1dec476b8a506bd3282b4caa" className={styles["imageview-1-1ec623ad1dec476b8a506bd3282b4caa"]} alt="imageview-1" src='https://i.ibb.co/5ndGgWv/image-43.png' />
+<div id="layer-7-e40be4a1db6f411badd3daaaa6a23ab2" className={styles["layer-7-e40be4a1db6f411badd3daaaa6a23ab2"]} >
+<div id="text-2-4c956812f0924dd6883960ae2e196e56" className={styles["text-2-4c956812f0924dd6883960ae2e196e56"]} >[Text]</div>
+<div id="text-3-6573859ad3c14b6aa50dba8c23c235a8" className={styles["text-3-6573859ad3c14b6aa50dba8c23c235a8"]} >[Text]</div>
+</div>
+</div>
+<div id="layer-6-f88fa5d3b8f84d5ebf9cc1ee470cc674" className={styles["layer-6-f88fa5d3b8f84d5ebf9cc1ee470cc674"]} >
+<img id="imageview-1-426a82062dc849239830770e04a6e400" className={styles["imageview-1-426a82062dc849239830770e04a6e400"]} alt="imageview-1" src='https://i.ibb.co/5ndGgWv/image-43.png' />
+<div id="layer-7-1c9cbe5e05ef4a14abf6afeaa5ef6081" className={styles["layer-7-1c9cbe5e05ef4a14abf6afeaa5ef6081"]} >
+<div id="text-2-81ce031e9fc0412faa97b936c05bdb89" className={styles["text-2-81ce031e9fc0412faa97b936c05bdb89"]} >[Text]</div>
+<div id="text-3-a6d3e4b332264777a9cb8c66d69f416b" className={styles["text-3-a6d3e4b332264777a9cb8c66d69f416b"]} >[Text]</div>
+</div>
+</div>
+<div id="layer-6-8384106e4c6e44c1bfbb442ce7f5426f" className={styles["layer-6-8384106e4c6e44c1bfbb442ce7f5426f"]} >
+<img id="imageview-1-8543fa810dc44cf3a08c93f0fd7f127d" className={styles["imageview-1-8543fa810dc44cf3a08c93f0fd7f127d"]} alt="imageview-1" src='https://i.ibb.co/5ndGgWv/image-43.png' />
+<div id="layer-7-94d9a8a3afd64c49a0a25cf8d4799040" className={styles["layer-7-94d9a8a3afd64c49a0a25cf8d4799040"]} >
+<div id="text-2-0cbba948a36044d8b1d74c5464bc4437" className={styles["text-2-0cbba948a36044d8b1d74c5464bc4437"]} >[Text]</div>
+<div id="text-3-e369f1305d2642be8f6a03f52ded1de9" className={styles["text-3-e369f1305d2642be8f6a03f52ded1de9"]} >[Text]</div>
+</div>
+</div>
+</div>
+<div id="layer-5-032aaa21aa9049d5b50e5db8f51ef292" className={styles["layer-5-032aaa21aa9049d5b50e5db8f51ef292"]} >
+<div id="layer-6-d9cc51660af5431195106d03e36322d9" className={styles["layer-6-d9cc51660af5431195106d03e36322d9"]} >
+<img id="imageview-1-88a63a15278143b5ae5046716c979675" className={styles["imageview-1-88a63a15278143b5ae5046716c979675"]} alt="imageview-1" src='https://i.ibb.co/5ndGgWv/image-43.png' />
+<div id="layer-7-fcf22f9ced894ebe942a604fdf508492" className={styles["layer-7-fcf22f9ced894ebe942a604fdf508492"]} >
+<div id="text-2-5ae0bd99c8294b90b742961ff573356e" className={styles["text-2-5ae0bd99c8294b90b742961ff573356e"]} >[Text]</div>
+<div id="text-3-596d059848ba402e9fc27fdb444357d5" className={styles["text-3-596d059848ba402e9fc27fdb444357d5"]} >[Text]</div>
+</div>
+</div>
+<div id="layer-6-41affe6da4c8405d894ba0bda6e9afd7" className={styles["layer-6-41affe6da4c8405d894ba0bda6e9afd7"]} >
+<img id="imageview-1-c3e0465fa7e34ce292c93762152035d3" className={styles["imageview-1-c3e0465fa7e34ce292c93762152035d3"]} alt="imageview-1" src='https://i.ibb.co/5ndGgWv/image-43.png' />
+<div id="layer-7-a916c99f36d04b4b8513c330ddfd9aeb" className={styles["layer-7-a916c99f36d04b4b8513c330ddfd9aeb"]} >
+<div id="text-2-17d0a2e43e2448acaa688a206c36d082" className={styles["text-2-17d0a2e43e2448acaa688a206c36d082"]} >[Text]</div>
+<div id="text-3-e7046d8dc97d4e41be81bd8f4219ec19" className={styles["text-3-e7046d8dc97d4e41be81bd8f4219ec19"]} >[Text]</div>
+</div>
+</div>
+<div id="layer-6-f9244be879544a99931204e34a61cca2" className={styles["layer-6-f9244be879544a99931204e34a61cca2"]} >
+<img id="imageview-1-54e03a073dad4f159de29d4765134da7" className={styles["imageview-1-54e03a073dad4f159de29d4765134da7"]} alt="imageview-1" src='https://i.ibb.co/5ndGgWv/image-43.png' />
+<div id="layer-7-d3fb73bdcffe4c0c847e62ccf7dfd5a0" className={styles["layer-7-d3fb73bdcffe4c0c847e62ccf7dfd5a0"]} >
+<div id="text-2-462783c978774e5ea53bdd34d4351be3" className={styles["text-2-462783c978774e5ea53bdd34d4351be3"]} >[Text]</div>
+<div id="text-3-a16c8dde929c4621ad972c7e1aafc2f4" className={styles["text-3-a16c8dde929c4621ad972c7e1aafc2f4"]} >[Text]</div>
+</div>
+</div>
+</div>
+        </div>
+    );
+}
+
+export default Screen2;
+            
